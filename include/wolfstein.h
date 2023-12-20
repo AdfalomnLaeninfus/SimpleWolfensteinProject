84ef74b68349ab424d021c4bd5f071d0212bcce3
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2023-12-19 22:27:37 -0300

    Add PI values

--- include/wolfstein.h
+++ include/wolfstein.h
@@ -11,4 +11,8 @@
     #define MIN_WINDOW_WIDTH    800
     #define MIN_WINDOW_HEIGHT   600
 
+    #define PI          3.1415926535
+    #define PI_DIV_TWO  1.5707963267
+    #define PI_DIV_180  0.0174532925
+
 #endif
