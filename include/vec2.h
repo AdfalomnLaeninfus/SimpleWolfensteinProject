0c57b07f5df2ed756215039316883d8339f8750a
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2023-12-19 21:40:44 -0300

    Initial function and struct for Vector2

---
+++ include/vec2.h
@@ -0,0 +1,12 @@
+#ifndef _VEC2_H_
+#define _VEC2_H_
+
+typedef struct vec2 {
+    float x;
+    float y;
+} Vec2;
+
+float vec2_dot( Vec2 v1, Vec2 v2 );
+float vec2_lenght( Vec2 v1, Vec2 v2 );
+
+#endif
