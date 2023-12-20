371e284618be89384a26f2af9cd2b53ca3de2dbc
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2023-12-19 21:41:44 -0300

    Constants and imports for project

---
+++ include/wolfstein.h
@@ -0,0 +1,14 @@
+#ifndef _WOLFSTEIN_H_
+#define _WOLFSTEIN_H_
+
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <stdbool.h>
+
+    #include "SDL2/SDL.h"
+
+    #define WINDOW_TITLE        "Wolfstein Project"
+    #define MIN_WINDOW_WIDTH    800
+    #define MIN_WINDOW_HEIGHT   600
+
+#endif
