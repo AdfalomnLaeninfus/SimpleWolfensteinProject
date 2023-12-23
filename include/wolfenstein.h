ba7700b4b41d55f9d7429569dbc58a6184b2065c
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2023-12-23 02:37:08 -0300

    Add include header file of player

---
+++ include/wolfenstein.h
@@ -0,0 +1,22 @@
+#ifndef _WOLFSTEIN_H_
+#define _WOLFSTEIN_H_
+
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <stdbool.h>
+
+    #include "SDL2/SDL.h"
+    
+    #include <player.h>
+
+    #define WINDOW_TITLE        "Wolfenstein Project"
+    #define MIN_WINDOW_WIDTH    800
+    #define MIN_WINDOW_HEIGHT   600
+
+    #define PI          3.1415926535
+    #define PI_DIV_TWO  1.5707963267
+    #define PI_DIV_180  0.0174532925
+
+    #define MAP_SIZE 8
+
+#endif
