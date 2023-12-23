eca1dffc73440c3b37f59f902053e5918160649b
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2023-12-23 03:05:08 -0300

    Add some operations to vector2

--- include/vec2.h
+++ include/vec2.h
@@ -9,9 +9,11 @@ typedef struct vec2 {
 float vec2_dot( Vec2 v1, Vec2 v2 );
 float vec2_lenght( Vec2 v1 );
 
-Vec2 vec2_add( Vec2 v1, Vec2 v2 );
-Vec2 vec2_sub( Vec2 v1, Vec2 v2 );
-Vec2 vec2_mul( Vec2 v1, Vec2 v2 );
-Vec2 vec2_div( Vec2 v1, Vec2 v2 );
+Vec2 vec2_add( const Vec2 *v1, const Vec2 *v2 );
+Vec2 vec2_sub( const Vec2 *v1, const Vec2 *v2 );
+Vec2 vec2_mul( const Vec2 *v1, const Vec2 *v2 );
+Vec2 vec2_div( const Vec2 *v1, const Vec2 *v2 );
+
+Vec2 vec2_rotate( const Vec2 *v1, const float angle);
 
 #endif
