ecb46cb26d0113137a8a11cfb6a87f5b149cf18a
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2023-12-23 03:05:19 -0300

    Add some operations to vector2

--- src/vec2.c
+++ src/vec2.c
@@ -1,11 +1,51 @@
 #include <math.h>
 #include <vec2.h>
 
-float vec2_dot( Vec2 v1, Vec2 v2 )
+float vec2_dot( const Vec2 v1, const Vec2 v2 )
 {
     return ( v1.x * v2.x ) + ( v1.y * v2.y );
 }
 
-float vec2_lenght( Vec2 v1 ) {
+float vec2_lenght( const Vec2 v1 ) {
     return sqrtf( vec2_dot( v1, v1 ) );
+}
+
+Vec2 vec2_add( const Vec2 *v1, const Vec2 *v2 )
+{
+    return (Vec2) {
+        v1->x + v2->x,
+        v1->y + v2->y
+    };
+}
+
+Vec2 vec2_sub( const Vec2 *v1, const Vec2 *v2 )
+{
+    return (Vec2) {
+        v1->x - v2->x,
+        v1->y - v2->x
+    };
+}
+
+Vec2 vec2_mul( const Vec2 *v1, const Vec2 *v2 )
+{
+    return (Vec2) {
+        v1->x * v2->x,
+        v1->y * v2->y
+    };
+}
+
+Vec2 vec2_div( const Vec2 *v1, const Vec2 *v2 )
+{
+    return (Vec2) {
+        v1->x / v2->x,
+        v1->y / v2->y
+    };
+}
+
+Vec2 vec2_rotate( const Vec2 *v1, const float angle)
+{
+    return (Vec2) {
+        ( v1->x * cosf(angle) ) - ( v1->x * sinf(angle) ),
+        ( v1->y * sinf(angle) ) - ( v1->y * cosf(angle) )
+    };
 }
