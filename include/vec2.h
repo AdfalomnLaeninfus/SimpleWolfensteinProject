630f1e47e5bd4402b4f0ee82e22270c8cc6d795e
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2023-12-23 02:33:34 -0300

    Add operation functions for vector2 in header file

--- include/vec2.h
+++ include/vec2.h
@@ -9,4 +9,9 @@ typedef struct vec2 {
 float vec2_dot( Vec2 v1, Vec2 v2 );
 float vec2_lenght( Vec2 v1 );
 
+Vec2 vec2_add( Vec2 v1, Vec2 v2 );
+Vec2 vec2_sub( Vec2 v1, Vec2 v2 );
+Vec2 vec2_mul( Vec2 v1, Vec2 v2 );
+Vec2 vec2_div( Vec2 v1, Vec2 v2 );
+
 #endif
--- main.c
+++ main.c
@@ -4,7 +4,39 @@
 
 #include <wolfstein.h>
 
+typedef struct app {
+    SDL_Event event;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+
+    bool quit;
+} App;
+
 int main(int argc, char *argv[])
 {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    App app;
+
+    app.quit = false;
+
+    app.window = SDL_CreateWindow(
+        WINDOW_TITLE,
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        MIN_WINDOW_WIDTH, MIN_WINDOW_HEIGHT, SDL_WINDOW_SHOWN
+    );
+
+    app.renderer = SDL_CreateRenderer(app.window, -1, SDL_RENDERER_SOFTWARE);
+
+    while (app.quit) {
+        while (SDL_PollEvent(&app.event)) {
+            ;
+        }
+    }
+
+    SDL_DestroyRenderer(app.renderer);
+    SDL_DestroyWindow(app.window);
+    SDL_Quit();
+
     return 0;
 }
