5cb42e4c7924d8f32f245ff3cbc018c866a0ac3f
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2023-12-24 00:50:12 -0300

    Add some includes of app and player

--- include/wolfenstein.h
+++ include/wolfenstein.h
@@ -7,16 +7,26 @@
 
     #include "SDL2/SDL.h"
     
+    #include <app.h>
     #include <entity.h>
+    #include <player.h>
 
     #define WINDOW_TITLE        "Wolfenstein Project"
     #define MIN_WINDOW_WIDTH    800
     #define MIN_WINDOW_HEIGHT   600
 
-    #define PI          3.1415926535
-    #define PI_DIV_TWO  1.5707963267
-    #define PI_DIV_180  0.0174532925
+    #define PI              3.1415926535
+    #define PI_TIMES_TWO    6.2831853071
+    #define PI_DIV_TWO      1.5707963267
+    #define PI_DIV_180      0.0174532925
 
-    #define MAP_SIZE 8
+    #define MAP_SIZE_WIDTH  8
+    #define MAP_SIZE_HEIGHT 8
+    #define MAP_SIZE        64
+
+    #define WALL_WIDTH      64
+    #define WALL_HEIGHT     64
+
+    #define PLAYER_VELOCITY 10
 
 #endif
