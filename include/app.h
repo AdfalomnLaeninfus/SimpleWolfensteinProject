acdf86ab746e7c455243f039b774325829f6665d
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2023-12-24 01:15:46 -0300

    Add window ratio atribute

--- include/app.h
+++ include/app.h
@@ -3,10 +3,13 @@
 
 #include <wolfenstein.h>
 
+typedef Vec2 WindowRatio;
+
 typedef struct app {
     SDL_Event event;
     SDL_Window *window;
     SDL_Renderer *renderer;
+    WindowRatio window_ratio;
 
     bool quit;
 } App;
