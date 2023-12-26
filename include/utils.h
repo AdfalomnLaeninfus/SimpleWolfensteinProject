fcd9e02f25a413d82fbdf8e52fee5481f78c9c25
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2023-12-26 01:20:35 -0300

    Remove defintions from wolfenstein to utils

--- include/utils.h
+++ include/utils.h
@@ -3,6 +3,23 @@
 
 #include "app.h"
 
+#ifndef PI
+    #define PI              3.1415926535
+#endif
+
+#define PI_TIMES_TWO    6.2831853071
+#define PI_DIV_TWO      1.5707963267
+#define PI_DIV_180      0.0174532925
+
+#define MAP_SIZE_WIDTH  8
+#define MAP_SIZE_HEIGHT 8
+#define MAP_SIZE        64
+
+#define WALL_WIDTH      64
+#define WALL_HEIGHT     64
+
+#define PLAYER_VELOCITY .2
+
 float toRadians( float degrees );
 
 void draw_map( const int *map, App *app );
