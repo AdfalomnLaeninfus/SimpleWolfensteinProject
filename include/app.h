1a6d23bf4a5eeb0fc061a9d4fd05118d6660e328
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2023-12-27 22:58:41 -0300

    Change definitions for App type

--- include/app.h
+++ include/app.h
@@ -1,14 +1,22 @@
 #ifndef _APP_H_
 #define _APP_H_
 
-#include <wolfenstein.h>
+#include <vec2.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
 
 typedef Vec2 WindowRatio;
+
+typedef struct window_size {
+    int width;
+    int height;
+} WindowSize;
 
 typedef struct app {
     SDL_Event event;
     SDL_Window *window;
     SDL_Renderer *renderer;
+    WindowSize window_size;
     WindowRatio window_ratio;
 
     bool quit;
