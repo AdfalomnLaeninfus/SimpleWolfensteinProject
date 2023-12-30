5caf686e66e57306587a36cddb39d41cd7295dbc
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2023-12-29 22:07:24 -0300

    Implement projects functions

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #endif
 
 #include <wolfenstein.h>
+#include <stdint.h>
 
 const int map[MAP_SIZE] = {
     1, 1, 1, 1, 1, 1, 1, 1,
@@ -19,6 +20,7 @@ int main(int argc, char *argv[])
 {
     App app;
     EntityInstance_t player;
+
     Vec2 keysPressed = { 0.0f, 0.0f };
     Vec2 windowCenter = { 0.0f, 0.0f };
 
@@ -32,7 +34,7 @@ int main(int argc, char *argv[])
         WINDOW_TITLE,
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         MIN_WINDOW_WIDTH, MIN_WINDOW_HEIGHT,
-        SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
+        SDL_WINDOW_SHOWN
     );
 
     SDL_SetWindowMinimumSize(app.window, MIN_WINDOW_WIDTH, MIN_WINDOW_HEIGHT);
@@ -40,7 +42,7 @@ int main(int argc, char *argv[])
     app.renderer = SDL_CreateRenderer( app.window, -1, SDL_RENDERER_SOFTWARE );
     SDL_GetWindowSize( app.window, &app.window_size.width, &app.window_size.height );
 
-    app.keyboardKeys = SDL_GetKeyboardState(NULL);
+    app.keyboardKeys = (unsigned char *) SDL_GetKeyboardState(NULL);
 
     app.quit = false;
     while ( !app.quit )
@@ -55,10 +57,16 @@ int main(int argc, char *argv[])
                 case SDL_QUIT: app.quit = true;
                 case SDL_MOUSEMOTION:
                     int mouseX, mouseY;
+                    float mouseSensibility = 0.02f;
 
                     SDL_GetRelativeMouseState( &mouseX, &mouseY );
 
-                    set_entity_rotation( &player.transform, (Vec2) { player.transform.rotation.x + floorf(mouseX) * -0.002f, player.transform.rotation.y } );
+                    set_entity_rotation( &player.transform, (Vec2)
+                        {
+                            player.transform.rotation.x - sinf( mouseSensibility * -mouseX ),
+                            player.transform.rotation.y + cosf( mouseSensibility * mouseY )
+                        }
+                    );
 
                     break;
             }
@@ -69,11 +77,23 @@ int main(int argc, char *argv[])
         if ( app.keyboardKeys[SDL_SCANCODE_S] ) { keysPressed.y -= 1; }
         if ( app.keyboardKeys[SDL_SCANCODE_A] ) { keysPressed.x -= 1; }
 
-        set_entity_place( &player.transform, (Vec2) {
-                player.transform.position.x + (keysPressed.x * PLAYER_VELOCITY),
-                player.transform.position.y + (keysPressed.y * PLAYER_VELOCITY)
-            }
-        );
+        float pPosX, pPosY, pRotX, pRotY;
+
+        pPosX = player.transform.position.x;
+        pPosY = player.transform.position.y;
+        pRotX = player.transform.rotation.x;
+        pRotY = player.transform.rotation.y;
+
+
+        if ( keysPressed.y != 0.0f )
+        {
+            set_entity_place( &player.transform, (Vec2)
+                {
+                    pPosX + (sinf(pRotX) * (PLAYER_VELOCITY * keysPressed.y)),
+                    pPosY - (cosf(pRotX) * (PLAYER_VELOCITY * keysPressed.y))
+                }
+            );
+        }
 
         keysPressed.x = keysPressed.y = 0;
 
@@ -82,6 +102,17 @@ int main(int argc, char *argv[])
 
         draw_map( map, &app );
         draw_rect( &app, player.transform.position.x, player.transform.position.y, 12, 12);
+
+        SDL_SetRenderDrawColor( app.renderer, 255, 0, 255, 255 );
+        SDL_RenderDrawLine(
+            app.renderer,
+
+            pPosX + 5,
+            pPosY + 5,
+
+            pPosX + (sinf(pRotX) * 25),
+            pPosY - (cosf(pRotX) * 25)
+        );
 
         SDL_RenderPresent( app.renderer );
         SDL_Delay(16);
