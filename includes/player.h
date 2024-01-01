8e169f0e743220243a3ffde337034d7fd2d46866
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2024-01-01 19:20:19 -0300

    Rename folder of includes

---
+++ includes/player.h
@@ -0,0 +1,8 @@
+#ifndef _PLAYER_H_
+#define _PLAYER_H_
+
+#include "entity.h"
+
+typedef EntityPlayableInstance_t Player;
+
+#endif
---
+++ includes/vec2.h
@@ -0,0 +1,19 @@
+#ifndef _VEC2_H_
+#define _VEC2_H_
+
+typedef struct vec2 {
+    float x;
+    float y;
+} Vec2;
+
+float vec2_dot( Vec2 v1, Vec2 v2 );
+float vec2_lenght( Vec2 v1 );
+
+extern Vec2 vec2_add( const Vec2 *v1, const Vec2 *v2 );
+extern Vec2 vec2_sub( const Vec2 *v1, const Vec2 *v2 );
+extern Vec2 vec2_mul( const Vec2 *v1, const Vec2 *v2 );
+extern Vec2 vec2_div( const Vec2 *v1, const Vec2 *v2 );
+
+extern Vec2 vec2_rotate( const Vec2 *v1, const float angle);
+
+#endif
