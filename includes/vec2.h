144b0593c815ebffe9c27addc3cd9e2b151b608d
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2024-01-02 01:35:22 -0300

    Change type of arg for vec2 functions

--- includes/vec2.h
+++ includes/vec2.h
@@ -9,10 +9,10 @@ typedef struct vec2 {
 float vec2_dot( Vec2 v1, Vec2 v2 );
 float vec2_lenght( Vec2 v1 );
 
-extern Vec2 vec2_add( const Vec2 *v1, const Vec2 *v2 );
-extern Vec2 vec2_sub( const Vec2 *v1, const Vec2 *v2 );
-extern Vec2 vec2_mul( const Vec2 *v1, const Vec2 *v2 );
-extern Vec2 vec2_div( const Vec2 *v1, const Vec2 *v2 );
+extern Vec2 vec2_add( const Vec2 *v1, const float value );
+extern Vec2 vec2_sub( const Vec2 *v1, const float value );
+extern Vec2 vec2_mul( const Vec2 *v1, const float value );
+extern Vec2 vec2_div( const Vec2 *v1, const float value );
 
 extern Vec2 vec2_rotate( const Vec2 *v1, const float angle);
 
