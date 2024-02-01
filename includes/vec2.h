754bc7bb6a5eca52bb44f32f03fe67c41ab594db
YumemiReis <adrianoreispereira88@gmail.com>
2024-02-01 17:11:30 -0300

    change functions return

--- includes/vec2.h
+++ includes/vec2.h
@@ -5,16 +5,16 @@ typedef struct vec2f { float x, y; } Vec2f;
 typedef struct vec2i { int x, y; } Vec2i;
 
 float vec2_dot( Vec2f v1, Vec2f v2 );
-float vec2_lenght( Vec2f v1 );
+float vec2_lenght( Vec2f v );
 
-extern Vec2i vec2f_to_vec2i( Vec2i );
-extern Vec2f vec2i_to_vec2f( Vec2f );
+extern Vec2i vec2f_to_vec2i( Vec2f *v );
+extern Vec2f vec2i_to_vec2f( Vec2i *v );
 
-extern Vec2f vec2_add( const Vec2f *v1, const float value );
-extern Vec2f vec2_sub( const Vec2f *v1, const float value );
-extern Vec2f vec2_mul( const Vec2f *v1, const float value );
-extern Vec2f vec2_div( const Vec2f *v1, const float value );
+extern Vec2f vec2_add( const Vec2f *v, const float value );
+extern Vec2f vec2_sub( const Vec2f *v, const float value );
+extern Vec2f vec2_mul( const Vec2f *v, const float value );
+extern Vec2f vec2_div( const Vec2f *v, const float value );
 
-extern Vec2f vec2_rotate( const Vec2f *v1, const float angle);
+extern Vec2f vec2_rotate( const Vec2f *v, const float angle);
 
 #endif
--- src/vec2.c
+++ src/vec2.c
@@ -4,13 +4,13 @@
 float vec2_dot( const Vec2f v1, const Vec2f v2 ) { return ( v1.x * v2.x ) + ( v1.y * v2.y ); }
 float vec2_lenght( const Vec2f v1 ) { return sqrtf( vec2_dot( v1, v1 ) ); }
 
-Vec2f vec2i_to_vec2f( Vec2i *v ) { return ( Vec2f ) { ( int ) v->x, ( int ) v->y }; }
-Vec2i vec2f_to_vec2i( Vec2f *v ) { return ( Vec2i ) { ( float ) v->x, ( float ) v->y }; }
+Vec2f vec2i_to_vec2f( Vec2i *v ) { return ( Vec2f ) { ( float ) v->x, ( float ) v->y }; }
+Vec2i vec2f_to_vec2i( Vec2f *v ) { return ( Vec2i ) { ( int ) v->x, ( int ) v->y }; }
 
-Vec2f vec2_add( const Vec2f *v1, float value ) { return ( Vec2f ) { v1->x + value, v1->y + value }; }
-Vec2f vec2_sub( const Vec2f *v1, float value ) { return ( Vec2f ) { v1->x - value, v1->y - value }; }
-Vec2f vec2_mul( const Vec2f *v1, float value ) { return ( Vec2f ) { v1->x * value, v1->y * value }; }
-Vec2f vec2_div( const Vec2f *v1, float value ) { return ( Vec2f ) { v1->x / value, v1->y / value }; }
+Vec2f vec2_add( const Vec2f *v, float value ) { return ( Vec2f ) { v->x + value, v->y + value }; }
+Vec2f vec2_sub( const Vec2f *v, float value ) { return ( Vec2f ) { v->x - value, v->y - value }; }
+Vec2f vec2_mul( const Vec2f *v, float value ) { return ( Vec2f ) { v->x * value, v->y * value }; }
+Vec2f vec2_div( const Vec2f *v, float value ) { return ( Vec2f ) { v->x / value, v->y / value }; }
 
 Vec2f vec2_rotate( const Vec2f *v1, const float angle )
 {
