5e3e329599af6c159f0af6d69cf9dd38ef6f5198
YumemiReis <adrianoreispereira88@gmail.com>
2024-07-09 14:40:19 -0300

    Coded base program

--- main.c
+++ main.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+#if defined(_WIN32) || defined(WIN32)
+    #define SDL_MAIN_HANDLED
+#endif
+#include <SDL2/SDL.h>
+
+SDL_Event event;
+SDL_Window *window;
+SDL_Renderer *renderer;
+
+int running = 1;
+
+void handled_events(SDL_Event *event);
+
+int main(int argc, char **argv)
 {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow("Wolfstein", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+
+    SDL_SetWindowMinimumSize(window, 800, 600);
+
+    while (running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            handled_events(&event);
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+
     return 0;
+}
+
+void handled_events(SDL_Event *event)
+{
+    switch (event->type)
+    {
+        case SDL_QUIT:
+            running = !running;
+            break;
+    }
 }
